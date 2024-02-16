269623adf28db18584594d0f16703d56d98fd95d
Nameer Haider <nmeer5242@gmail.com>
2024-02-16 21:03:16 +0300

    renaming the project from packifyr to repackify

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Packifyr
+# repackify
 ___
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,18 @@
-console.log('test');
+import readJson from 'read-package-json';
+import path, { dirname } from 'path';
+
+const __dirname = path.resolve(dirname('./'), 'src');
+const parentDir = path.dirname(__dirname);
+readJson(
+  `${parentDir}/package.json`,
+  console.error,
+  false,
+  function (er: NodeJS.ErrnoException, data: Record<string, any>) {
+    if (er) {
+      console.error(er);
+      return;
+    }
+
+    console.error('the package data is', data);
+  },
+);
